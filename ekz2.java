89f1917a72919d7c57db7bf8df87742014224964
Popyg <tyl9ray@yandex.ru>
2022-01-06 02:37:00 +0300

    ekz2 fix

--- ekz2.java
+++ ekz2.java
@@ -3,11 +3,16 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.*;
 
 class Triangle {
-    private int a;
-    private int c;
+    private double a;
+    private double c;
 
 
-    public Triangle(int a, int b, int c) throws Exception {
+    public Triangle(double x_a, double y_a, double x_b, double y_b, double x_c, double y_c) throws Exception {
+
+        double a = Math.sqrt(Math.pow(Math.abs(x_a - x_b), 2) + Math.pow(Math.abs(y_a - y_b), 2));
+        double b = Math.sqrt(Math.pow(Math.abs(x_a - x_c), 2) + Math.pow(Math.abs(y_a - y_c), 2));
+        double c = Math.sqrt(Math.pow(Math.abs(x_b - x_c), 2) + Math.pow(Math.abs(y_b - y_c), 2));
+
         if ((a+b > c) && (a+c > b) && (b+c > a)) {
             if (a == b) {
                 this.a = a;
@@ -26,7 +31,10 @@ class Triangle {
         }
     }
 
-    public int perimeter() {
+
+
+
+    public double perimeter() {
         return a*2 + c;
     }
 
@@ -53,13 +61,41 @@ class Triangle {
     }
 }
 
+class TriangleUtils {
+    public static double generalPerimeter(Triangle[] triangles) {
+        double sum = 0;
+        for (Triangle triangle : triangles) {
+            sum += triangle.perimeter();
+        }
+        return sum;
+    }
+
+    public static boolean twoEquals(Triangle[] triangles) {
+        for (int i = 0; i < triangles.length; i++) {
+            for (int j = i+1; j < triangles.length; j++) {
+                if (triangles[i].equals(triangles[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
 
 
-public class ekz2 {
+public class sumfact {
     public static void main(String[] args) {
         try {
-            Triangle a = new Triangle(3, 3, 4);
+            Triangle a = new Triangle(1, 4, 4,1,1,1);
+            Triangle b = new Triangle(2, 5, 5,2,2,2);
+            Triangle c = new Triangle(3, 6, 6,3,3,3);
+            Triangle d = new Triangle(3, 6, 6,3,3,3);
+
             System.out.println(a.toString());
+            System.out.println(b.toString());
+            System.out.println(c.toString());
+
+            System.out.println(TriangleUtils.twoEquals(new Triangle[]{a,b,c,d}));
         } catch (Exception e) {
             System.out.println(e);
         }
